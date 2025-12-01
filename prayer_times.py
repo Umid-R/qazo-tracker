ca854b9c023ae8b6fc1410bade99dd5f8de35b69
Umid-R <umidraxmatullayev96@gmail.com>
2025-12-01 13:56:21 +0900

    Update prayer_times.py

--- prayer_times.py
+++ prayer_times.py
@@ -1,17 +1,26 @@
 import requests
 
+# Gets the cordinates of the city 
+def get_cor_city(city):
+    url = f"https://nominatim.openstreetmap.org/search?city={city}&format=json"
+    response = requests.get(url,headers = {"User-Agent": "Mozilla/5.0 (TelegramBot)"})  
+    data = response.json()
+    if not data:
+        return None
+    lat = data[0]["lat"]
+    lon = data[0]["lon"]
+    return lat, lon
 
-response = requests.get('https://ipinfo.io/json')
-data = response.json()
+# Gets the prayer times for the specific cordinates
+def get_by_cor(lat,lon):
+  madhab = 1 #  1 = Hanafi 
+  url = f"https://api.aladhan.com/v1/timings?latitude={lat}&longitude={lon}&method=2&school={madhab}"
+  response = requests.get(url)
+  response.raise_for_status()
+  data = response.json()
+  return (data['data']['timings'])
 
-# Coordinates come as a string "lat,lon"
-lat, lon = map(float, data['loc'].split(','))
-print(f"Latitude: {lat}, Longitude: {lon}")
 
-madhab = 1 #  1 = Hanafi
-url = f"https://api.aladhan.com/v1/timings?latitude={lat}&longitude={lon}&method=2&school={madhab}"
 
-response = requests.get(url)
-response.raise_for_status()
-data = response.json()
-print(data['data']['timings'])
+
+    

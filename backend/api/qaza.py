f40660cebefa649caadc127fe6a5437f0586e691
Umid-R <umidraxmatullayev2005@gmail.com>
2025-12-23 17:38:16 +0900

    update FastApi

--- backend/api/qaza.py
+++ backend/api/qaza.py
@@ -4,35 +4,35 @@ from backend.Database.qaza_stats import get_total_qazas, get_today_prayed_qazas,
 router = APIRouter()
 
 @router.get('/user_info/{userId}')
-def get_user(user_id: int):
+def get_user(userId: int):
     try:
-        info=get_user_info(user_id)
+        info=get_user_info(userId)
         return info
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/total/{userId}")
-def total_qazas(user_id: int):
+def total_qazas(userId: int):
     try:
-        total = get_total_qazas(user_id)
+        total = get_total_qazas(userId)
         return {"total_qazas": total}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
     
 @router.get("/today_prayed/{userId}")
-def today_prayed(user_id: int):
+def today_prayed(userId: int):
     try:
-        total=get_today_prayed_qazas(user_id)
+        total=get_today_prayed_qazas(userId)
         return {"today_prayed": total}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
     
-@router.get('/qazas_stats/{userId}')
-def get_qazas_stats(user_id:int):
+@router.get('/breakdown/{userId}')
+def get_qazas_stats(userId:int):
     try:
-        stats=qazas_rating(user_id)
+        stats=qazas_rating(userId)
         return stats
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))

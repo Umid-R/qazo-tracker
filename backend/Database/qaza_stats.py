ebfed7d21609cebf62aed4d33021ea0370da12a7
Umid-R <umidraxmatullayev96@gmail.com>
2026-01-06 17:02:25 +0900

    update fastapi

--- backend/Database/qaza_stats.py
+++ backend/Database/qaza_stats.py
@@ -2,7 +2,7 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
 from datetime import  datetime, timedelta, time, date
-
+import random
 
 load_dotenv()
 
@@ -151,40 +151,55 @@ def get_prayers_stats(user_id: int):
 
 print(get_prayers_stats(1207972222))
 
-
 def get_weekly_activity(user_id: int):
     today = date.today()
     start_date = today - timedelta(days=6)
 
+    # fetch all prayer logs for last 7 days
     res = (
         Client
         .table("qazas")
         .select("time_prayed")
         .eq("user_id", user_id)
-        .eq("is_qaza", False)
         .gte("time_prayed", start_date.isoformat())
         .execute()
     )
 
-    prayed_dates = {
-        row["time_prayed"][:10]
-        for row in res.data
-        if row["time_prayed"]
-    }
-
-    days_map = ["M", "T", "W", "T", "F", "S", "S"]
-
-    result = []
+    # create last 7 days list with default False (red)
+    week_status = []
     for i in range(7):
-        day = today - timedelta(days=6 - i)
-        result.append({
-            "day": days_map[day.weekday()],
-            "active": day.isoformat() in prayed_dates
+        day = start_date + timedelta(days=i)
+        day_name = day.strftime("%a")
+        week_status.append({
+            "day": day_name,
+            "status": False  # red by default
         })
 
-    return result
+    # mark True (green) if at least one prayer exists
+    for entry in res.data:
+        log_day = entry['time_prayed'][:10]
+        log_day = date.fromisoformat(log_day)
+        idx = (log_day - start_date).days
+        if 0 <= idx < 7:
+            week_status[idx]["status"] = True
+
+    return week_status
+
 print(get_weekly_activity(1207972222))
 
+
+def get_profile_quotes():
+    res = (
+        Client
+        .table("profile_quotes")
+        .select("quote")
+        .execute()
+    )
+    quote=random.choice(res.data)
+    return quote
+
+
+print(get_profile_quotes())
 
 
 
--- backend/api/qaza.py
+++ backend/api/qaza.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from backend.Database.qaza_stats import get_total_qazas, get_prayers_stats,get_user_info,qazas_rating,get_weekly_activity
+from backend.Database.qaza_stats import get_total_qazas, get_prayers_stats,get_user_info,qazas_rating,get_weekly_activity, get_profile_quotes
 
 router = APIRouter()
 
@@ -54,3 +54,10 @@ def get_weekly_stats(userId: int):
         raise HTTPException(status_code=500, detail=str(e))
     
     
+@router.get("/quotes")
+def get_quotes():
+    try:
+        quote=get_profile_quotes()
+        return quote
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
